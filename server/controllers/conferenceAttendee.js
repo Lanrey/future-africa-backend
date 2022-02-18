d7e5a57100b89e5023b013faf4a5b6b999bd6601
Olusola Akinsulere <olusola.akinsulere@Terra-042.local>
2022-02-18 17:15:11 +0100

    feat: Added Attendee Controller

---
+++ server/controllers/conferenceAttendee.js
@@ -0,0 +1,30 @@
+import { serverError, serverResponse } from '../helper/serverResponse';
+import Attendee from '../database/models/attendee';
+
+/**
+ * @export
+ * @class doctor
+ */
+class Attendees {
+  /**
+   *
+   * @param {req} req
+   * @param {res} res
+   * @returns {object} attendee creation
+   */
+
+  static async addAttendee(req, res) {
+    try {
+      const attendeeBody = req.body;
+
+      const attendeeResult = await Attendee.createAttendee(attendeeBody);
+
+      return serverResponse(req, res, 201, { data: attendeeResult });
+    } catch (error) {
+      console.log(error);
+      return serverError(req, res, error);
+    }
+  }
+}
+
+export default Attendees;
